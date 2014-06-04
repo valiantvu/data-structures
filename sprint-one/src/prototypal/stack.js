12f255b51030413f958a4ed53a39b745e1f99bf5
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-04 16:33:31 -0700

    Completed prototypal for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
-var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  this.storage[this.len] = val;
+  this.len++;
+};
 
+stackMethods.pop = function() {
+  if (this.len > 0) {
+    this.len--;
+  }
+
+  var res = this.storage[this.len];
+  delete this.storage[this.len];
+  return res;
+};
+
+stackMethods.size = function() {
+  return this.len;
+};
+
+var makeStack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var newS = Object.create(stackMethods);
+  newS.len = 0;
+  newS.storage = {};
+  return newS;
+};
