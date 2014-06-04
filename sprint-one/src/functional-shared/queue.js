02da8e95183988efb8e52a592d2310c9fd606fa4
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-04 16:32:21 -0700

    Completed functional shared instantiation for queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,30 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQ = {len: 0, first: 0, last: 0, storage: {}};
+  return _.extend(newQ, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val) {
+    this.storage[this.last] = val;
+    this.len++;
+    this.last++;
+  },
+  dequeue: function() {
+    if (this.len > 0) {
+      this.len--;
+    }
+
+    var res = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return res;
+  },
+  size: function() {
+    return this.len;
+  }
+};
 
 
 
