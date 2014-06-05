6cc6417b59dccbf7cbee0b7a1bb023a2518f8425
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-05 10:35:23 -0700

    Solve set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (!this.contains(item)) {
+    this._storage[item] = true;
+  }
 };
 
 setPrototype.contains = function(item){
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+  if (this.contains(item)) {
+    delete this._storage[item];
+  }
 };
 
 /*
