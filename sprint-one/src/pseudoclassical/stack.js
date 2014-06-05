7c3478b661d45fde80fb4ce49728a7f84a2af143
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-04 20:29:43 -0700

    Solve stack with pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.len = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.push = function(val) {
+  this.storage[this.len] = val;
+  this.len++;
+};
+
+Stack.prototype.pop = function() {
+  if(this.len > 0) {
+    this.len--;
+    var res = this.storage[this.len];
+    delete this.storage[this.len];
+    return res;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.len;
+};
 
