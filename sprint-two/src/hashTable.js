1db3121ba5af4c1487cf08aacce27de744af08d6
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-05 11:33:43 -0700

    Solve hash table including collisions!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,24 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  for(var i = 0; i < this._limit; i++){
+    this._storage.set(i, {});
+  }
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  this._storage.get(i)[k] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  this._storage.get(i)[k] = null;
 };
 
 
