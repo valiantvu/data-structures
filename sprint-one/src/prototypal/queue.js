8001491e3975ae32ab5b6f788e40d367d5935fc9
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-04 17:41:11 -0700

    Solve queue for prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQ = Object.create(queueMethods);
+  newQ.len = 0;
+  newQ.first = 0;
+  newQ.last = 0;
+  newQ.storage = {};
+  return newQ;
 };
 
 var queueMethods = {};
 
-
+queueMethods.enqueue = function(val) {
+  this.storage[this.last] = val;
+  this.last++;
+  this.len++;
+};
+queueMethods.dequeue = function() {
+  if (this.len > 0) {
+    this.len--;
+  }
+  var res = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return res;
+};
+queueMethods.size = function() {
+  return this.len;
+};
