cc44f7393a43fa5f33f28f16916d6b1ec384d648
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-04 21:37:05 -0700

    Implement linkedlist to pass all tests!

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+
+    if(list.tail !== null) {
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    if(list.head !== null) {
+      var res = list.head.value;
+      list.head = list.head.next;
+      return res;
+    }
   };
 
   list.contains = function(target){
+    var found = (list.head.value === target);
+
+    var current = list.head;
+
+    while((current !== list.tail) && !found) {
+      current = current.next;
+      if (current.value === target) {
+        found = true;
+      }
+    }
+
+    return found;
   };
 
   return list;
