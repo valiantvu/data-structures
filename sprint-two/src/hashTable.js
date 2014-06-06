933d0799a50310b376fed44e4b5d13170d8295f2
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-06 14:06:26 -0700

    Refactor hashtable to use arrays for collisions instead of objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,23 +2,35 @@ var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
   for(var i = 0; i < this._limit; i++){
-    this._storage.set(i, {});
+    this._storage.set(i, []);
   }
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.get(i)[k] = v;
+  this._storage.get(i).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i)[k];
+  var tupArr = this._storage.get(i);
+  for(var j = 0; j < tupArr.length; j++) {
+    if(tupArr[j][0] === k){
+      return tupArr[j][1];
+    }
+  }
+  return null;
+
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.get(i)[k] = null;
+  var tupArr = this._storage.get(i);
+  for(var j = 0; j < tupArr.length; j++) {
+    if(tupArr[j][0] === k){
+      tupArr = tupArr.splice(j, 1);
+    }
+  }
 };
 
 
