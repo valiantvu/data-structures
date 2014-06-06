f30c59f74c49744dbcb133f60e16c9f4f40e6675
Hack Reactor Students <michelle.thi.vu@gmail.com>
2014-06-05 21:32:54 -0700

    Solve BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,53 @@
 var makeBinarySearchTree = function(value){
+  var bst = Object.create(bstMethods);
+  bst.right = null;
+  bst.left = null;
+  bst.value = value;
+  return bst;
 };
 
+var bstMethods = {};
+
+bstMethods.insert = function(val) {
+  var newBST = makeBinarySearchTree(val);
+  if(val < this.value) {
+    if(this.left) {
+      this.left.insert(val);
+    } else {
+      this.left = newBST;
+    }
+  } else {
+    if(this.right) {
+      this.right.insert(val);
+    } else {
+      this.right = newBST;
+    }
+  }
+};
+
+bstMethods.contains = function(val) {
+  var found = (this.value === val);
+  if (val < this.value) {
+    if(this.left) {
+      found = this.left.contains(val);
+    }
+  } else if (val > this.value) {
+    if(this.right) {
+      found = this.right.contains(val);
+    }
+  }
+  return found;
+};
+
+bstMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.right) {
+    this.right.depthFirstLog(callback);
+  }
+  if(this.left) {
+    this.left.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
