2f35b8ed57291e9201aee3a80a1f40866a4b2285
valiantvu <michelle.thi.vu@gmail.com>
2014-07-04 15:54:44 -0700

    Autosizing Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,11 +4,38 @@ var HashTable = function(){
   for(var i = 0; i < this._limit; i++){
     this._storage.set(i, []);
   }
+  this._count = 0;
+  this._over = false;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.get(i).push([k, v]);
+  this._count++;
+  if (this._count > (3/4) * this._limit) {
+    this.rehash(2);
+  }
+  if (this._count > (1/4) * this._limit) {
+    this._over = true;
+  }
+};
+
+HashTable.prototype.rehash = function (doubleOrHalf) {
+  this._count = 0;
+  this._over = false;
+  this._limit *= doubleOrHalf;
+  var copy = this._storage;
+  this._storage = makeLimitedArray(this._limit);
+
+  for(var i = 0; i < this._limit; i++){
+    this._storage.set(i, []);
+  }
+  var thisHash = this;
+  copy.each(function(tupArr) {
+    _.each(tupArr, function(tup) {
+      thisHash.insert(tup[0], tup[1]);
+    });
+  });
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -31,10 +58,10 @@ HashTable.prototype.remove = function(k){
       tupArr = tupArr.splice(j, 1);
     }
   }
+  this._count--;
+
+  if (this._over && (this._count < (1/4) * this._limit)) {
+    this.rehash(0.5);
+  }
+
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
